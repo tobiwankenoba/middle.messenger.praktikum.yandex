0393ea879d59766e9523459a3079d00b2f9460af
Roman Ovechkin <r.ovechkin@cian.ru>
2024-09-28 00:57:44 +0700

    перенос eslint

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,20 @@ export default [
       },
     },
   },
+  {
+    rules: {
+      "react/jsx-filename-extension": "off",
+      "import/extensions": "off",
+      "import/no-extraneous-dependencies": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/ban-ts-comment": "structuredClone",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
