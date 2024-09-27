a6642ce33e6c6f818d84f26c99130d789b8cceda
Roman Ovechkin <r.ovechkin@cian.ru>
2024-09-28 01:09:56 +0700

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -14,4 +16,6 @@ export default [
       },
     },
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.eslintRecommended,
 ];
