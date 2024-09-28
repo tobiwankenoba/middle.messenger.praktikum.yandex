2f5aed645a9e836b660f3298b901f1b40a12c935
Roman Ovechkin <r.ovechkin@cian.ru>
2024-09-28 16:57:30 +0700

    еще поправил nice to have

--- src/utils/getFieldFormError.ts
+++ src/utils/getFieldFormError.ts
@@ -7,18 +7,18 @@ export const getFieldFormError = (name: string, isValid: boolean): string => {
 
   switch (name) {
     case EFormFieldNames.Password:
-      return "Не выполнены условия пароля";
+      return "Не выполнены условия пароля: минимум 1 большая буква и цифра";
     case EFormFieldNames.Phone:
-      return "Неверный формат телефона";
+      return "Неверный формат телефона: может начинаться с + и иметь от 10 до 15 цифр";
     case EFormFieldNames.SecondName:
-      return "Неверный формат фамилии";
+      return "Неверный формат фамилии: первая буква заглавная без пробелов и цифр";
     case EFormFieldNames.FirstName:
-      return "Неверный формат имени";
+      return "Неверный формат имени: первая буква заглавная без пробелов и цифр";
     case EFormFieldNames.Login:
     case EFormFieldNames.DisplayName:
-      return "Неверный логин";
+      return "Неверный логин: от 3 до 20 символов";
     case EFormFieldNames.Email:
-      return "Неверный формат почты";
+      return "Неверный формат почты: должна быть «собака» (@)";
     case EFormFieldNames.PasswordRepeat:
       return "Пароль не совпадает";
     default:
